5a39aa51dd290c9a9417d832cd453f3b08bc6fb0
Emi Andrada <eandrada@ibm.com>
2023-03-25 03:22:58 -0300

    initial commit

--- ProductManager.js
+++ ProductManager.js
@@ -1,13 +1,15 @@
 export default class ProductManager {
+    #products
+    #productId
     constructor(){
-        this.products = []
-        this.productId=1
+        this.#products = []
+        this.#productId=1
     }
 
     addProduct(title,description,price,thumbnail,code,stock){
-        const prodWithSameCode=this.products.findIndex((prod)=>prod.code===code)
-        if(!title||!description||!price||!thumbnail||!code||!stock||prodWithSameCode!==-1){
-            if(prodWithSameCode!==-1){
+        const prodWithSameCode=this.#products.find((prod)=>prod.code===code)
+        if(!title||!description||!price||!thumbnail||!code||!stock||prodWithSameCode){
+            if(prodWithSameCode){
                 console.log("Error el codigo de producto ya existe");
             }else{
                 console.log("Error Alguno de los campos esta incompleto");
@@ -20,18 +22,18 @@ export default class ProductManager {
             thumbnail,
             code,
             stock,
-            id:this.productId
+            id:this.#productId
         }
-            this.products.push(product)
-            this.productId++  
+            this.#products.push(product)
+            this.#productId++  
         }
     }
     getProducts(){
-        return this.products
+        return this.#products
     }
     getProductById(id){
-        const productFindById=this.products.findIndex((prod)=>prod.id===id)
-        if(productFindById===-1){
+        const productFindById=this.#products.find((prod)=>prod.id===id)
+        if(!productFindById){
             console.log("Not Found");
         }else{
             return productFindById
--- main.js
+++ main.js
@@ -7,6 +7,8 @@ console.log(productManager.getProducts());
 productManager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25)
 console.log(productManager.getProducts()); 
 productManager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25)
-console.log(productManager.getProductById(15)); 
+console.log(productManager.getProductById(2)); 
+console.log(productManager.getProducts());
+
 
 
